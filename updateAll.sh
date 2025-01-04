da31f6ae0345728949dc05c4f2d17523869264c5
Adrián Ferrer Tarí <adrian20ferrer@gmail.com>
2025-01-04 09:52:24 +0100

    Some major change in the script using rsync instead of cp

--- updateAll.sh
+++ updateAll.sh
@@ -16,5 +16,15 @@ packages=(
 
 for element in "${packages[@]}"
 do
-  cp -r "/home/$USER/.config/$element" "/home/$USER/dotfiles/$element"
+  if [ -d "/home/$USER/.config/$element" ]; then
+    # Si existe en .config, copiarlo a dotfiles
+    rsync -a --delete "/home/$USER/.config/$element/" "/home/$USER/dotfiles/$element/"
+    echo "Sincronizado $element de .config a dotfiles"
+  elif [ -d "/home/$USER/dotfiles/$element" ]; then
+    # Si no existe en .config pero sí en dotfiles, crear un enlace simbólico
+    ln -sf "/home/$USER/dotfiles/$element" "/home/$USER/.config/"
+    echo "Creado enlace simbólico de $element desde dotfiles a .config"
+  else
+    echo "No se encontró $element ni en .config ni en dotfiles"
+  fi
 done
